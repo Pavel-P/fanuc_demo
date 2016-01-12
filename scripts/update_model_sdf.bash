f55aa390fd2e69e157b3a52c0ca245516ee40474
Pavel-P <pavapil@gmail.com>
2016-01-12 12:57:06 -0500

    Improved launch file format and demo ui

--- scripts/update_model_sdf.bash
+++ scripts/update_model_sdf.bash
@@ -5,7 +5,9 @@ for dir in $(rospack find fanuc_demo)/models/*; do
     xacro_file="$(rospack find fanuc_demo)/models/$dir/$dir.urdf.xacro"
     if [ -f "$xacro_file" ]; then
         rosrun xacro xacro.py "$xacro_file" -o tmp
+	
         gz sdf -p tmp > "$(rospack find fanuc_demo)/models/$dir/model.sdf"
+        #gzsdf print tmp > "$(rospack find fanuc_demo)/models/$dir/model.sdf" #for gazebo version <=2 
         rm tmp
     fi
 done
--- src/demo.cpp
+++ src/demo.cpp
@@ -9,10 +9,13 @@
 #include <descartes_moveit/moveit_state_adapter.h>
 // Includes the descartes trajectory type we will be using
 #include <descartes_trajectory/axial_symmetric_pt.h>
+
 #include <descartes_trajectory/cart_trajectory_pt.h>
 // Includes the planner we will be using
 #include <descartes_planner/dense_planner.h>
 #include <descartes_planner/sparse_planner.h>
+
+#include <descartes_trajectory/cartesian_interpolator.h>
 
 //subscriber msgs
 #include <std_msgs/String.h>
@@ -71,11 +74,10 @@ toROSJointTrajectory(const TrajectoryVec& trajectory,
     return result;
 }
 
-bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory)
+bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory, const std::string& trajectory_ns)
 {
     // Create a Follow Joint Trajectory Action Client
-    //  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac("joint_trajectory_action", true);
-    actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac("joint_trajectory_controller/follow_joint_trajectory", true);
+    actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac(trajectory_ns, true);
     if (!ac.waitForServer(ros::Duration(2.0)))
     {
         ROS_ERROR("Could not connect to action server");
@@ -100,19 +102,25 @@ bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory)
 
 int main (int argc, char **argv)
 {
-    /* Descartes Model Initialization */
 
-    const std::string robot_description = argv[1];
-    const std::string group_name = argv[2];
-    const std::string world_frame = argv[3];
-    const std::string tcp_frame = argv[4];
+    /*SET GLOBAL PROPERTIES*/
 
-    ros::init(argc, argv, "simple_message_demo");
+    double Z_OFFSET = .33;
+    std::string robot_description = argv[1];
+    std::string group_name = argv[2];
+    std::string world_frame = argv[3];
+    std::string tcp_frame = argv[4];
+    std::string joint_trajectory_ns = argv[5];
+
+    /* INITIALIZE ROS NODE AND DESCARTES MODEL */
+
+    ros::init(argc, argv, "fanuc_demo");
     ros::AsyncSpinner spinner(1);
     spinner.start();
     ros::NodeHandle nh;
 
-    descartes_core::RobotModelPtr model (new descartes_moveit::MoveitStateAdapter);
+    //	    descartes_core::RobotModelPtr model (new descartes_moveit::MoveitStateAdapter);
+    descartes_moveit::MoveitStateAdapterPtr model (new descartes_moveit::MoveitStateAdapter);
     if (!model->initialize(robot_description, group_name, world_frame, tcp_frame))
     {
         ROS_INFO("Could not initialize robot model");
@@ -121,65 +129,166 @@ int main (int argc, char **argv)
     model->setCheckCollisions(true);
     ROS_INFO("Robot initialized successfully!");
 
-    //descartes_planner::DensePlanner planner;
-    descartes_planner::SparsePlanner planner;
+    descartes_planner::DensePlanner planner;
+
+    descartes_trajectory::CartesianInterpolator cart_interp;
+    cart_interp.initialize(model, .2, .1, .01);
+
     planner.initialize(model);
 
     // Get Joint Names
     std::vector<std::string> j_names;
     nh.getParam("controller_joint_names", j_names);
 
+    /* CONSOLE SPECIFIC VARIABLES */
+
+    enum TrajectoryType {JOINT, CART, AXIAL};
+    TrajectoryType traj_flag = CART;
+    descartes_trajectory::AxialSymmetricPt::FreeAxis axis = descartes_trajectory::AxialSymmetricPt::Z_AXIS;
+
     TrajectoryVec points;
     TrajectoryVec results;
     std::vector<double> times;
 
-    bool successful_exection = false;
+
+    printf("***Descartes Trajectory Tool***\n");
+    printf("Type h for help\n");
     while (true)
     {
+        printf("> ");
+        using namespace descartes_core;
+        using namespace descartes_trajectory;
         char inbuf[1024];
-        float f1, f2, f3, f4, f5, f6, f7;
+        double f1, f2, f3, f4, f5, f6, f7;
         if (NULL == fgets(inbuf, sizeof(inbuf), stdin)) break;
         // skip leading whitespace
         char *ptr = inbuf;
         while (isspace(*ptr)) ptr++;
-
         if ('q' == *ptr)
         {
+            printf("Quitting...\n");
             break;
         }
-        else if (7 == sscanf(ptr, "%f %f %f %f %f %f %f",
-                    &f1, &f2, &f3, &f4, &f5, &f6, &f7))
+        if ('h' == *ptr)
         {
-            using namespace descartes_core;
-            using namespace descartes_trajectory;
-            if (points.empty())
-            {   
-                //dummy point
-                std::vector<double> joints;
-                for (int q = 0; q < 6; q++) joints.push_back(0);
-                TrajectoryPtPtr dummy_pt = TrajectoryPtPtr(new JointTrajectoryPt(joints));
-                points.push_back(dummy_pt);
-            }
-            TrajectoryPtPtr next_point = TrajectoryPtPtr(new AxialSymmetricPt( f1, f2, f3, f4, f5, f6, M_PI/2.0, AxialSymmetricPt::Z_AXIS));
-            points.push_back(next_point);
-            times.push_back(f7);
-            using namespace descartes_core;
-            if (points.size() > 1 )
+            printf("Help:\n");
+            continue;
+        }
+        else if ('c' == *ptr)
+        {
+            traj_flag = CART;
+            printf("Trajectory type set to Cartesian\n");
+            continue;
+        }
+        else if ('j' == *ptr)
+        {
+            traj_flag = JOINT;
+            printf("Trajectory type set to Joint\n");
+            continue;
+        }
+        else if ('a' == *ptr)
+        {
+            traj_flag = AXIAL;
+            printf("Trajectory type set to Axial Symmetric\n");
+            continue;
+        }
+        else if ('r' == *ptr)
+        {
+            printf("Resetting trajectories...\n");
+            results.clear();
+            points.clear();
+            times.clear();
+            continue;
+        }
+        else if ('i' == *ptr)
+        {
+            TrajectoryVec interp_pts;
+            if (cart_interp.interpolate(points, interp_pts))
             {
-                times.push_back(1.0);
-                if (planner.planPath(points) && planner.getPath(results))
-                {
-                    trajectory_msgs::JointTrajectory joint_solution = toROSJointTrajectory(results, *model, j_names, times);
-                    successful_exection = executeTrajectory(joint_solution);
-                }
+                std::vector<double> fake_times(interp_pts.size(), 1.0);
+                times = fake_times;
                 points.clear();
-                times.clear();
-                if (successful_exection)
-                {
-                    points.push_back(results.back());
-                }
-                results.clear();
+                points = interp_pts;
+                printf("Interpolation successful\n");
             }
+            else
+            {
+                printf("Interpolation failed\n");
+            }
+            continue;
+        }
+        else if ('e' == *ptr)
+        {
+            printf("Executing...\n");
+
+            //add dummy start point (needed for gazebo, which drops first point)
+            std::vector<double> joints;
+            model->getState()->copyJointGroupPositions(group_name, joints);
+            TrajectoryPtPtr current_pos = TrajectoryPtPtr(new JointTrajectoryPt(joints));
+            results.insert(results.begin(),current_pos);
+            times.push_back(1.0);
+            if (!results.empty())
+            {
+                trajectory_msgs::JointTrajectory joint_solution = toROSJointTrajectory(results, *model, j_names, times);
+                executeTrajectory(joint_solution, joint_trajectory_ns);
+
+                ROS_ERROR("Time size: %ld Results size: %ld", times.size(), results.size());	
+            }
+            results.erase(results.begin());
+            times.pop_back();
+            continue;
+        }
+        else if ('p' == *ptr)
+        {
+            printf("Planning...\n");
+            if (planner.planPath(points) && planner.getPath(results))
+            {
+                printf("Planned path successfully\n");
+            }
+        }
+        else if (7 == sscanf(ptr, "%lf %lf %lf %lf %lf %lf %lf",
+                    &f1, &f2, &f3, &f4, &f5, &f6, &f7)) {
+            printf("Adding points...\n");
+            TrajectoryPtPtr next_point;
+            std::vector<double> joint_pt;
+            joint_pt.clear();
+            switch(traj_flag)
+            {
+                case JOINT:
+                    {
+                        joint_pt.push_back(f1);
+                        joint_pt.push_back(f2);
+                        joint_pt.push_back(f3);
+                        joint_pt.push_back(f4);
+                        joint_pt.push_back(f5);
+                        joint_pt.push_back(f6);
+                        next_point = TrajectoryPtPtr(new JointTrajectoryPt(joint_pt));
+                    }; break;
+                case CART:
+                    {
+                        f4 = -f4 * M_PI / 180;
+                        f5 = f5 * M_PI / 180;
+                        f6 = -f6 * M_PI / 180;
+                        Eigen::Affine3d pose;
+                        Eigen::Affine3d ax = Eigen::Affine3d(Eigen::AngleAxisd(f4, Eigen::Vector3d(1, 0, 0)));
+                        Eigen::Affine3d ay = Eigen::Affine3d(Eigen::AngleAxisd(f5, Eigen::Vector3d(0, 1, 0)));
+                        Eigen::Affine3d az = Eigen::Affine3d(Eigen::AngleAxisd(f6, Eigen::Vector3d(0, 0, 1)));
+                        pose = Eigen::Translation3d(f1, f2, f3 + Z_OFFSET) * ax * ay * az;
+
+                        next_point = TrajectoryPtPtr(new CartTrajectoryPt(TolerancedFrame(pose))) ;
+                    }; break;
+                case AXIAL:
+                    {
+                        next_point = TrajectoryPtPtr(new AxialSymmetricPt( f1, f2, f3 + Z_OFFSET,
+                                    f4, f5, f6, M_PI/2.0, AxialSymmetricPt::Z_AXIS));
+                    }; break;
+            }
+            points.push_back(std::move(next_point));
+            times.push_back(std::move(f7));
+
+            printf("Added point(s)\n");
+            printf("Point Vector Size: %ld, Time Vector Size %ld\n", points.size(), times.size());
+            continue;
         }
     }
 }
